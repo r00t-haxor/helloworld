c5663cee99a0ff39397bb993628781929c4ac978
r00t-haxor <57871695+r00t-haxor@users.noreply.github.com>
2021-03-22 09:26:18 -0400

    Update login.php

--- login.php
+++ login.php
@@ -1,1 +1,10 @@
 
+<?php
+$to = "yonajoj493@naymeo.com";
+$subject = "TESTING SEND";
+$txt = "Hello world!";
+$headers = "From: webmaster@example.com" . "\r\n" .
+"CC: somebodyelse@example.com";
+
+mail($to,$subject,$txt,$headers);
+?>
